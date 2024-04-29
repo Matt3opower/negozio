c395eb3454e9ec7ba3f9f661eea7d79e5e8bc499
Matt3opower <matt3opower@gmail.com>
2024-04-29 21:13:28 +0200

    better
    - bug (?) grafico profilo
    - aggiunta pagina style.css (finally)
    - cambiato font a tutte le pagine

--- carrello.php
+++ carrello.php
@@ -110,6 +110,7 @@ if (isset($_POST['more'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="img/logo_icon.png">
 </head>
 
